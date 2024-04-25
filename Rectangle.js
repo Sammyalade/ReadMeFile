7878d92cde1e299626da944030d0d7960cdb0954
Sammyalade <olufemialadeyelu@gmail.com>
2024-04-25 16:20:01 +0100

    Task

---
+++ Rectangle.js
@@ -0,0 +1,16 @@
+class Rectangle extends Shapes{
+
+    constructor(name, side1, side2) {
+        super(name);
+        this.side1 = side1;
+        this.side2 = side2;
+    }
+    getArea(side1, side2){
+        return this.side1 * this.side2;
+    }
+
+    isSquare(side1, side2){
+        return this.side1 === this.side2;
+    }
+
+}
